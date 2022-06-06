7a09dbdbc17e82d1659a90ae1a05bce712330c87
TheGoodlike13 <henrisssss@hotmail.com>
2022-06-06 09:42:58 +0300

    Deleted run2
    I was not using it anyways. It can remain as doc in run.

--- run.bat
+++ run.bat
@@ -2,3 +2,7 @@
 :: stops any running server and runs it again (in console)
 :: you will see server launch output in console
 gradlew libertyStop libertyRun
+
+:: stops any running server and starts it again (in the background)
+:: you will not see all the output in the console - have to check logs
+:: gradlew libertyStop libertyStart
--- run2.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-:: stops any running server and starts it again (in the background)
-:: you will not see all the output in the console - have to check logs
-gradlew libertyStop libertyStart
